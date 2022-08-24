a8563bd00874c9b16c801ca8398885cf903a30d3
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-08-24 15:39:12 -0300

    Creating event platform

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+import Lesson from "./components/Lesson"
+import Sidebar from "./components/Sidebar"
+import Video from "./components/Video"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Sidebar/>
+      <Lesson/>
+      <Video/>
+    </>
+  )
+}
+
+export default App
